381ea51f2bc0fa4f22d21fc6ce8d552065e7145e
K.Yamashiro <okazbb@gmail.com>
2016-02-25 13:27:56 +0900

    一部カラムのupdate時の対応

--- behaviors/i18n_column.php
+++ behaviors/i18n_column.php
@@ -4,13 +4,15 @@
  */
 class I18nBehavior extends ModelBehavior {
 
+    //言語用
     const DEFAULT_LANGUAGE = 'ja';
     const LANGUAGE_EN = 'en';
     const LANGUAGE_TW = 'tw';
 
+    //設定値
     public $i18n_find = true; //trueの時、findで国際化カラムを取得する
     public $i18n_column = 'i18n'; //国際化情報を保存するDBカラム名
-    public $language = array();
+    public $language = array(); //言語
 
     /*
       i18nカラムのデータ格納形式
@@ -27,16 +29,23 @@ class I18nBehavior extends ModelBehavior {
     */
 
     /**
-     * 言語リスト
+     * 使用言語リスト
      */
     function languageList(){
         return $this->language;
     }
 
+    /**
+     * 定義済みの全言語を使用する
+     */
     function setAllLanguage(){
         $this->language = $this->getAllLanguage();
     }
 
+    /**
+     * 定義済み言語リスト
+     * @return array
+     */
     function getAllLanguage(){
         return array(
             self::LANGUAGE_EN,
@@ -44,10 +53,18 @@ class I18nBehavior extends ModelBehavior {
         );
     }
 
+    /**
+     * カラム置き換えしない
+     * @param $model
+     */
     function notReplaceColumn(&$model){
         $this->settings[$model->name]['replace_column'] = false;
     }
 
+    /**
+     * バックエンド用の指定
+     * @param $model
+     */
     function useForBackend(&$model){
         $this->setAllLanguage();
         $this->notReplaceColumn($model);
@@ -83,8 +100,13 @@ class I18nBehavior extends ModelBehavior {
 
         //言語
         if(empty($this->language)){
-		$this->language[] = DEFAULT_LANGUAGE;
-	}
+            $this->language[] = DEFAULT_LANGUAGE;
+        }
+
+        //バックエンド用の指定
+        if(Configure::read('Public.ApplicationType') == CONST_APPLICATION_BACKEND){
+            $this->useForBackend($model);
+        }
 
         //find時のカラム置き換え
         if(!isset($this->settings[$model->name]['replace_column'])){
@@ -124,77 +146,68 @@ class I18nBehavior extends ModelBehavior {
      * @param mixed $results
      * @param bool $primary
      * @return mixed
+     * @link http://www.skyarc.co.jp/engineerblog/entry/_cakephpafterfind_phpercakephp_cakephpafterfindphpwarning_cake_3_count_appmodel_afterfind3.html
      */
     function afterFind(&$model, $results, $primary) {
 
-        if($this->i18n_find){
-            if($this->settings[$model->name]['replace_column']){
-                foreach($results as $key=>&$val){
+        if(isset($results[0])){
 
-                    if(!isset($val[$model->name])) continue;
-                    $i18n_results = array();    //結果セットに追加する内容
-                    $i18n_data = array();       //serialize済み多言語カラムデータ
-
-                    if(isset($val[$model->name][$this->i18n_column])){
-                        $i18n_data = unserialize($val[$model->name][$this->i18n_column]);
-                    }
-
-                    foreach($this->language as $language){
-
-                        if($language == self::DEFAULT_LANGUAGE) continue; //ベース言語は無視
-
-                        if(array_key_exists($language, $i18n_data)){
-
-                            $i18n_row = $i18n_data[$language];
-
-                            foreach($this->settings[$model->name]['fields'] as $val2){
-                                if(isset($i18n_row[$val2]) && !empty($i18n_row[$val2])){
-                                    $i18n_results[$val2] = $i18n_row[$val2];
-                                }
-                            }
-
-                        }
-                    }
-
-                    $val[$model->name] = array_merge($val[$model->name], $i18n_results);
+            if(isset($results[0][$model->alias])){
+                //array(0 => array(model => array( field => value )))
+                foreach($results as $key=>$val){
+                    $this->set_split_i18n_value($model, $results[$key][$model->alias]);
                 }
+
             } else {
-
-                foreach($results as $key=>&$val){
-
-                    if(!isset($val[$model->name])) continue;
-                    $i18n_results = array();    //結果セットに追加する内容
-                    $i18n_data = array();       //serialize済み多言語カラムデータ
-
-                    if(isset($val[$model->name][$this->i18n_column])){
-                        $i18n_data = unserialize($val[$model->name][$this->i18n_column]);
-                    }
-
-                    foreach($this->language as $language){
-
-                        if($language == self::DEFAULT_LANGUAGE) continue; //ベース言語は無視
-
-                        if(array_key_exists($language, $i18n_data)){
-
-                            $i18n_row = $i18n_data[$language];
-                            foreach($this->settings[$model->name]['fields'] as $val2){
-                                $i18n_results[$val2 . '_' . $language] = isset($i18n_row[$val2]) ? $i18n_row[$val2] : null;
-                            }
-
-                        }else {
-
-                            foreach($this->settings[$model->name]['fields'] as $val3){
-                                $i18n_results[$val3 . '_' . $language] = null;
-                            }
-                        }
-                    }
-
-                    $val[$model->name] = array_merge($val[$model->name], $i18n_results);
+                //array(0 => array(field => value)))
+                foreach($results as $key2=>$val2){
+                    $this->set_split_i18n_value($model, $results[$key2]);
                 }
             }
+
+        } elseif(is_array($results)) {
+            // array(field => value)
+            $this->set_split_i18n_value($model, $results);
         }
 
         return $results;
+    }
+
+    /**
+     * i18nカラムの更新用データをセット
+     * 既存のデータに更新分データを上書き
+     */
+    function override_i18n_column(&$model){
+
+        if(!empty($model->data[$model->name]['id'])){
+
+            $i18n_field_exists = false;
+
+            foreach($this->getAllLanguage() as $language){ //save時は全言語をチェック
+                if($language == self::DEFAULT_LANGUAGE) continue; //ベース言語は無視
+                foreach($this->settings[$model->name]['fields'] as $fields){
+                    if(isset($model->data[$model->name][$fields . '_' . $language])){
+                        $i18n_field_exists = true;
+                        break 2;
+                    }
+                }
+            }
+
+            if(!$i18n_field_exists) return;
+
+            $params = array(
+                'conditions'    => array('id'=>$model->data[$model->name]['id']),
+                'fields'        => array($this->i18n_column),
+                'contain'       => false,
+            );
+            $data = $model->find('first', $params);
+
+            $model->data[$model->name] = array_merge_ex(
+                $data[$model->name],
+                $model->data[$model->name]
+            );
+
+        }
     }
 
     /**
@@ -212,6 +225,7 @@ class I18nBehavior extends ModelBehavior {
      * @return mixed|void
      */
     function beforeSave(&$model){
+        $this->override_i18n_column($model);
         $this->serialize_i18n_data($model);
     }
 
@@ -221,13 +235,14 @@ class I18nBehavior extends ModelBehavior {
      */
     function set_validate_i18n_column(&$model){
 	    
-	    foreach($this->getAllLanguage() as $language){ //save時は全言語をチェック
+	foreach($this->getAllLanguage() as $language){ //save時は全言語をチェック
 
             if($language == self::DEFAULT_LANGUAGE) continue; //ベース言語は無視
 
             foreach($this->settings[$model->name]['fields'] as $fields){
                 if(isset($model->validate[$fields])){
                     $model->validate[$fields . '_' . $language] = $model->validate[$fields];
+                    //TODO autoConvert対応
                 }
             }
         }
@@ -254,5 +269,38 @@ class I18nBehavior extends ModelBehavior {
         }
 
         $model->data[$model->name][$this->i18n_column] = !empty($i18n_data) ? serialize($i18n_data) : array();
+    }
+
+    /**
+     * @param $model
+     * @param $row
+     */
+    function set_split_i18n_value(&$model, &$row){
+        $i18n_results = array();    //結果セットに追加する内容
+        $i18n_data = array();       //serialize済み多言語カラムデータ
+
+        if(isset($row[$this->i18n_column])){
+
+            $i18n_data = unserialize($row[$this->i18n_column]);
+
+            foreach($this->language as $language){
+                if($language == self::DEFAULT_LANGUAGE) continue; //ベース言語は無視
+                if(array_key_exists($language, $i18n_data)){
+
+                    $i18n_row = $i18n_data[$language];
+
+                    foreach($this->settings[$model->name]['fields'] as $val){
+                        if(isset($i18n_row[$val]) && !empty($i18n_row[$val])){
+                            if($this->settings[$model->name]['replace_column']){
+                                $i18n_results[$val] = $i18n_row[$val];
+                            } else {
+                                $i18n_results[$val . '_' . $language] = $i18n_row[$val];
+                            }
+                        }
+                    }
+                }
+            }
+            $row = array_merge($row, $i18n_results);
+        }
     }
 }
